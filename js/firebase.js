3f9317996360a9a4e509005f1d455387c6a61749
tiianprb0 <136343253+tiianprb0@users.noreply.github.com>
2025-04-17 20:11:45 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebas
 import { getFirestore, doc, getDoc, updateDoc, arrayUnion, setDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAxRrX7mCDURme97jVZCQZKZKOxhPK0u4c",
-    authDomain: "todolist-7e983.firebaseapp.com",
-    projectId: "todolist-7e983",
-    storageBucket: "todolist-7e983.appspot.com",
-    messagingSenderId: "708377851604",
-    appId: "1:708377851604:web:6855f3e4f69b3b49916d41",
-    measurementId: "G-LHJY8EZ8F3"
+    apiKey: "kode",
+    authDomain: "kode",
+    projectId: "kode",
+    storageBucket: "kode",
+    messagingSenderId: "kode",
+    appId: "kode",
+    measurementId: "scrollfesnuk"
 };
 
 const app = initializeApp(firebaseConfig);
